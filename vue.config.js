f3192903dffd3af6834d59ba201b206f5a0918f0
qq_36765018 <qq_36765018@devide>
2023-07-30 10:17:00 +0800

    Sun Jul 30 10:17:00 CST 2023 inscode

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     port: process.env.DEV_SERVER_PORT || 8080,
     proxy: {
       '^/api': {
-        target: 'http://localhost:3000',
+        target: 'http://mapi.bizhangjie.top',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/',
